a7f42c7394c99d30196a7d7aa5a7656c576a0f42
luiicode <luiseduardo.corte35@gmail.com>
2026-08-17 23:05:12 -0600

    :sparkles: feat(app): integrate MenuBar and Footer components into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Outlet} from 'react-router-dom'
+import {MenuBar} from "./components/shared/MenuBar.tsx";
+import {Footer} from "./components/shared/Footer.tsx";
 
 export function App() {
     return (
-        <div>
-            <main>
+        <div className={'flex flex-col w-full min-h-screen overflow-y-auto'}>
+            <MenuBar/>
+            <main className={'flex flex-1'}>
                 <Outlet/>
             </main>
+            <Footer/>
         </div>
     );
 }
